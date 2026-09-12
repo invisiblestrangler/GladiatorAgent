80a56b14c12075f1792787aec0f913a6d7a65b1a
invisiblestrangler <104713936+invisiblestrangler@users.noreply.github.com>
2026-09-13 02:05:01 +0700

    feat: export runtime primitives

---
+++ src/gladiator/runtime/__init__.py
@@ -0,0 +1,11 @@
+from .context import ContextBudget, ObservationLimitResult, ObservationLimiter
+from .decision import DecisionBroker, DecisionRequest, DecisionResult
+
+__all__ = [
+    "ContextBudget",
+    "ObservationLimitResult",
+    "ObservationLimiter",
+    "DecisionBroker",
+    "DecisionRequest",
+    "DecisionResult",
+]
