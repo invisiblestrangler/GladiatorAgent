8e470577fd681727e361bdc9ba44cfd18604989c
invisiblestrangler <104713936+invisiblestrangler@users.noreply.github.com>
2026-09-15 02:29:21 +0700

    feat: export Codex OAuth model transport

--- src/gladiator/models/__init__.py
+++ src/gladiator/models/__init__.py
@@ -1,3 +1,9 @@
+from .codex_oauth import CodexOAuthStreamingModel, extract_chatgpt_account_id, oauth_token_expiration
 from .openai_streaming import OpenAICompatibleStreamingModel
 
-__all__ = ["OpenAICompatibleStreamingModel"]
+__all__ = [
+    "CodexOAuthStreamingModel",
+    "OpenAICompatibleStreamingModel",
+    "extract_chatgpt_account_id",
+    "oauth_token_expiration",
+]
